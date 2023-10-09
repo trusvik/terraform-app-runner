42e3d9cfc0a73d3158b2d93488e7c448f6cc123a
Glenn Bech <glenn.bech@gmail.com>
2023-10-09 23:08:02 +0200

    Update apprunner.tf
    auto deploy

--- apprunner.tf
+++ apprunner.tf
@@ -13,6 +13,6 @@ resource "aws_apprunner_service" "service" {
       image_identifier      = var.image
       image_repository_type = "ECR"
     }
-    auto_deployments_enabled = false
+    auto_deployments_enabled = true
   }
 }
